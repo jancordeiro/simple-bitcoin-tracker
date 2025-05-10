e70b3d6631889173612b7be646e1a58568d979df
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2025-05-10 20:28:30 -0300

    Update install-deb.sh

--- install-deb.sh
+++ install-deb.sh
@@ -4,10 +4,9 @@ echo "📦 Checking dependencies..."
 
 sudo apt update
 
-# Install required dependencies
-sudo apt install -y gir1.2-gtk-4.0 python3-requests \
-    gir1.2-graphene-1.0 python3-certifi python3-charset-normalizer \
-    python3-idna python3-chardet
+# Install required dependencies for GTK 3 + WebKit2 + Requests
+sudo apt install -y python3-gi gir1.2-gtk-3.0 gir1.2-webkit2-4.0 python3-requests \
+    python3-certifi python3-charset-normalizer python3-idna
 
 echo "🚀 Installing Simple Bitcoin Tracker..."
 sudo dpkg -i simple-bitcoin-tracker_1.0.deb
